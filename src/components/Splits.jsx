5b97fe74522caf19afdf9e23f5260057dfc08316
Isha Ghaisas <ighaisas@iu.edu>
2025-06-02 10:28:50 -0400

    added styles to splits component

--- src/components/Splits.jsx
+++ src/components/Splits.jsx
@@ -9,11 +9,13 @@ const Splits = () => {
                 <h1 className="title">Choose your split!</h1>
                 <div className="container"><h2 className="subtitle">Or create one</h2>
                     <FaPlus className="icon-add" /></div>
-                {
-                    defaultSplits.map((split) => (
-                        <p>{`${split[0]} : ${split[1]}`}</p>
-                    ))
-                }
+                <div className="default-splits">
+                    {
+                        defaultSplits.map((split, index) => (
+                            <button key={index} className="split-btn">{`${split[0]} : ${split[1]}`}</button>
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
